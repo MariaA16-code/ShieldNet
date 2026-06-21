025a8a7ed2446596dc9dce0df9e5697fbf6bb9bd
MariaA16-code <70147167@student.uol.edu.pk>
2026-06-21 13:33:58 +0500

    ready for render deployment

--- backend/app.py
+++ backend/app.py
@@ -19,7 +19,7 @@ password = quote_plus(os.getenv('DB_PASSWORD'))
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     f"mysql+pymysql://{os.getenv('DB_USER')}:{password}"
-    f"@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 )
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
